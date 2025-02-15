87e44bc62f7198a869a0486f87b4915a7c13e622
Andrey Egorov <and7643@gmail.com>
2025-02-15 20:47:13 +0300

    fix: load image 3

--- src/index.ts
+++ src/index.ts
@@ -18,60 +18,48 @@ class SheepHandler {
   woolColor: Color = { r: 0, g: 0, b: 0 };
   skinColor: Color = { r: 0, g: 0, b: 0 };
 
-  loadSheepImg = () => {
-    console.log("sheep start")
-    const img = this.sheepImg.get()[0] as HTMLImageElement;
-    this.canvas.width = img.width;
-    this.canvas.height = img.height;
-  
-    this.ctx.drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight, 0, 0, img.width, img.height);
-    this.currentPixels = this.ctx.getImageData(0, 0, img.width, img.height);
-    this.originalPixels = this.ctx.getImageData(0, 0, img.width, img.height);
-    console.log(this.originalPixels)
-    console.log("sheep end")
-  }
-
-  loadWoolImg = () => {
-    if (!this.currentPixels) {
-      // Because if I does not control order images will be messed up... I dont know why...
-      console.log("w")
-      setTimeout(this.loadWoolImg, 100);
-      return;
-    }
-    console.log("wool start");
-    const img = $("#sheepWoolMask").get()[0] as HTMLImageElement;
-    this.ctx.drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight, 0, 0, img.width, img.height);
-    this.woolMask = this.ctx.getImageData(0, 0, img.width, img.height);
-    console.log(this.woolMask)
-    console.log("wool end");
-  }
-
-  loadSkinImg = () => {
-    if (!this.woolMask) {
-      console.log("s")
-      setTimeout(this.loadSkinImg, 100);
-      return;
-    }
-    console.log("skin start")
-    const img = $("#sheepSkinMask").get()[0] as HTMLImageElement;
-    this.ctx.drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight, 0, 0, img.width, img.height);
-    this.skinMask = this.ctx.getImageData(0, 0, img.width, img.height);
-    console.log(this.skinMask)
-    console.log("skin end");
-  }
-
   loadImgs = () => {
     imagesloaded.makeJQueryPlugin($);
-    // @ts-ignore
-    this.sheepImg.imagesLoaded(this.loadSheepImg);
+
+    // Load sheep image
+    const sheepImage = new Image();
+    sheepImage.onload = () => {
+      console.log("sheep start")
+      const img = sheepImage;
+      this.canvas.width = img.width;
+      this.canvas.height = img.height;
+    
+      this.ctx.drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight, 0, 0, img.width, img.height);
+      this.currentPixels = this.ctx.getImageData(0, 0, img.width, img.height);
+      this.originalPixels = this.ctx.getImageData(0, 0, img.width, img.height);
+      console.log(this.originalPixels)
+      console.log("sheep end")
+    }
+    sheepImage.src = "images/sheep.jpg";
 
     // Load wool mask
-    // @ts-ignore
-    $("#sheepWoolMask").imagesLoaded(this.loadWoolImg);
+    const woolMaskImage = new Image();
+    woolMaskImage.onload = () => {
+      console.log("wool start");
+      const img = woolMaskImage;
+      this.ctx.drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight, 0, 0, img.width, img.height);
+      this.woolMask = this.ctx.getImageData(0, 0, img.width, img.height);
+      console.log(this.woolMask)
+      console.log("wool end");
+    }
+    woolMaskImage.src = "images/sheep_wool_mask.jpg";
 
     // Load skin mask
-    // @ts-ignore
-    $("#sheepSkinMask").imagesLoaded(this.loadSkinImg);
+    const skinMaskImage = new Image();
+    skinMaskImage.onload = () => {
+      console.log("skin start")
+      const img = skinMaskImage;
+      this.ctx.drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight, 0, 0, img.width, img.height);
+      this.skinMask = this.ctx.getImageData(0, 0, img.width, img.height);
+      console.log(this.skinMask)
+      console.log("skin end");
+    }
+    skinMaskImage.src = "images/sheep_skin_mask.jpg";
   }
    
   updateSheep = () => {
@@ -79,7 +67,7 @@ class SheepHandler {
       alert("ERROR: images did not load correctly :( Try refresh the page.")
       return;
     }
-    console.log(this.originalPixels, this.woolMask, this.skinMask)
+    // console.log(this.originalPixels, this.woolMask, this.skinMask)
   
     for(var I = 0, L = this.originalPixels.data.length; I < L; I += 4) {
       const wm = this.woolMask.data[I] / 255;
