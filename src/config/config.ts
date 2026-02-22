e6b2e5b799ed2bc8ccae34cf02afff08600addd6
Andrey Egorov <and7643@gmail.com>
2026-02-23 00:36:46 +0300

    feat: almost added dragon

--- src/config/config.ts
+++ src/config/config.ts
@@ -18,9 +18,9 @@ export async function getColors(filename: string): Promise<ColorConfig> {
   return $.getJSON(filename) // BUG wrong promise
 }
 
-const SupportedConfigs = ["bat", "sheep", "horntail"];
+const SupportedConfigs = ["bat", "sheep", "horntail", "dragon2"];
 
-async function getConfig(name:string): Promise<Config | undefined> {
+async function getConfig(name: string): Promise<Config | undefined> {
   if (!SupportedConfigs.includes(name)) {
     return undefined;
   }
@@ -33,13 +33,13 @@ async function getConfig(name:string): Promise<Config | undefined> {
 export async function setupConfig(): Promise<void> {
   // Look for config name
   let name = window.location.pathname.split("/")[1];
-  var cfg =  await getConfig(name);
+  var cfg = await getConfig(name);
   if (cfg == undefined) {
     console.error("ERROR: no config found")
     return;
   }
   config = cfg as Config;
-  
+
   // Validating
   if (!config || config.backgroundImgURL == "")
     console.error("ERROR: no background image in config")
